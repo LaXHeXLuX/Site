6d776c727cd4baf4f8e25ee29a7f35e459c29065
LaXHeXLuX <laas.lumberg@gmail.com>
2024-05-20 06:42:50 +0300

    refactored level making to be more dynamic, added level2

--- games/mathInventor/mathInventor.js
+++ games/mathInventor/mathInventor.js
@@ -1,22 +1,37 @@
 import {} from '../../commonTools.js';
 
-function makeLevel1() {
-    const div = document.createElement('div');
-    div.className = 'level';
-    div.id = 'level1';
-    div.innerHTML = `<p>The beginning of the world of <span class="numbers">numbers</span>, 
+const levelElements = {
+    '1': `<p>The beginning of the world of <span class="numbers">numbers</span>, 
     they say, is and always has been <span class="zero">0</span>, so this is the number you'll start with. <br>
-    You'll see your current number at the bottom of the screen at all times</p>
-<p>The first gadget they have introduced to us is an 
+    You'll see your current number at the bottom of the screen at all times.</p>
+    <p>The first gadget they have introduced to us is an 
     <span class="incrementor">incrementor</span> (<span class="incrementor"> ' </span>). <br>
     It takes in an integer, and returns the next integer after it.  <br>
-    You'll find it (and all other gadgets from now on) at the bottom of the screen, under your number</p> <br>
-    <p class="task">Your first task is to increment your number to <span style="font-weight: bold">10</span></p>`;
+    You'll find it (and all other gadgets from now on) at the bottom of the screen, under your number.</p> <br>
+    <p class="task">Your first task is to increment your number to <span style="font-weight: bold">10</span></p>`,
+    '2': `<p>Good job! You may notice that by starting with a number <span style="font-weight: bold;">a</span> and using the 
+    <span class="incrementor">incrementor</span> <span style="font-weight: bold;">b</span> 
+    times, you get a number that can be expressed as <span style="font-weight: bold;">a</span> 
+    <span class="plus">+</span> <span style="font-weight: bold;">b</span>.</p> <br>
+    <p><span class="plus">+</span> (called <span style="color: darkorange;">plus</span>) is your second gadget.</p>
+    <p>You can think of it as a functional symbol that takes in two numbers, 
+    <span style="font-weight: bold;">a</span> and <span style="font-weight: bold;">b</span>, and returns a value that is 
+    bigger than <span style="font-weight: bold;">a</span> by the amount of <span style="font-weight: bold;">b</span></p> <br>
+    <p class="task">Your second task is to add with numbers you have already unlocked, growing your number to at least <span style="font-weight: bold">100</span></p>`,
+    '3': ''
+};
+
+function makeLevel(number) {
+    const div = document.createElement('div');
+    div.className = 'level';
+    div.id = 'level' + number;
+    div.innerHTML = levelElements[number];
     return div;
 }
 
 function makeFooter() {
     const footer = document.createElement('footer');
+    footer.id = 'footer';
 
     const number = document.createElement('p');
     number.id = 'number';
@@ -36,22 +51,44 @@ function makeFirstGadget() {
     const p = document.createElement('p');
     p.innerHTML = '&nbsp\'&nbsp';
     p.className = 'incrementor';
-
+    
+    const number = document.getElementById('number');
     increment.appendChild(p);
     increment.addEventListener('click', () => {
-        const number = document.getElementById('number');
         number.innerHTML = parseInt(number.innerHTML) + 1;
+        checkContent(number);
     });
 
     gadgets.appendChild(increment);
     return gadgets;
 }
 
+function checkContent(element) {
+    let value = parseInt(element.innerHTML);
+    if (value === 10 && currentLevel === 1) {
+        appendLevel();
+    };
+    
+}
+
+function appendLevel() {
+    currentLevel = currentLevel + 1;
+    const number = currentLevel.toString();
+    document.getElementById('levels').appendChild(makeLevel(number));
+}
+
+let currentLevel = 0;
+
 const letsGetStarted = document.getElementById('letsGetStarted');
 letsGetStarted.addEventListener('click', () => {
-    document.body.appendChild(makeLevel1());
-    document.body.appendChild(makeFooter());
-    document.getElementsByTagName('footer')[0].appendChild(makeFirstGadget());
+    const div = document.createElement('div');
+    div.id = 'levels';
+    document.body.appendChild(div);
+    appendLevel();
+
+    const footer = makeFooter();
+    document.body.appendChild(footer);
+    footer.appendChild(makeFirstGadget());
 
     letsGetStarted.remove();
 });
